a7f71286c6c81cb39ac29637ccb0f5a5de91d841
Traorelacina <traorelac01@gmail.com>
2024-09-25 01:48:31 +0000

    inscription et connexion

--- recycl/src/pages/LoginPage.js
+++ recycl/src/pages/LoginPage.js
@@ -4,10 +4,10 @@ import '../styles/LoginForm.css'; // Assurez-vous que le chemin est correct
 
 const LoginPage = () => {
   return (
-    <div className="login-container">
-      <h1 className="login-title">Bienvenue sur Recyclab ! Connectez-vous.</h1>
-      <form className="login-form">
-        <div className="form-group">
+    <div className="login-page-container">
+      <h1 className="login-page-title">Bienvenue sur Recyclab ! Connectez-vous.</h1>
+      <form className="login-page-form">
+        <div className="login-form-group">
           <label htmlFor="identifier">Adresse e-mail ou numéro de tél</label>
           <input
             type="text"
@@ -15,7 +15,7 @@ const LoginPage = () => {
             placeholder="Entrez votre email ou numéro de téléphone"
           />
         </div>
-        <div className="form-group">
+        <div className="login-form-group">
           <label htmlFor="password">Mot de passe</label>
           <input
             type="password"
@@ -23,12 +23,12 @@ const LoginPage = () => {
             placeholder="Entrez votre mot de passe"
           />
         </div>
-        <button type="submit" className="login-button">Se connecter</button>
+        <button type="submit" className="login-page-button">Se connecter</button>
       </form>
-      <div className="form-links">
-        <Link to="/signup" className="create-account">Créer un compte?</Link>
-        <span className="link-separator">•</span>
-        <Link to="/forgot-password" className="forgot-password">Mot de passe oublié ?</Link>
+      <div className="login-form-links">
+        <Link to="/signup" className="login-create-account">Créer un compte?</Link>
+        <span className="login-link-separator">•</span>
+        <Link to="/forgot-password" className="login-forgot-password">Mot de passe oublié ?</Link>
       </div>
     </div>
   );
--- recycl/src/pages/SignupForm.js
+++ recycl/src/pages/SignupForm.js
@@ -8,90 +8,119 @@ const SignupPage = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [commune, setCommune] = useState('');
+  const [status, setStatus] = useState('');
+  const [termsAccepted, setTermsAccepted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Logique pour gérer l'inscription ici (API call, validation, etc.)
-    if (password === confirmPassword) {
-      console.log('Inscription:', { firstName, lastName, email, password, commune });
-      // Appeler l'API pour enregistrer l'utilisateur
-    } else {
-      alert("Les mots de passe ne correspondent pas.");
+    if (!termsAccepted) {
+      alert("Veuillez accepter les termes de confidentialité.");
+      return;
     }
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne correspondent pas.");
+      return;
+    }
+
+    console.log('Inscription:', { firstName, lastName, email, password, commune, status });
   };
 
   return (
-    <div className="signup-container">
-    
-      
-      <form onSubmit={handleSubmit}>
-      <h2 style={{ fontSize: '3em', position: 'absolute', top: '20px', left: '20px' }}>S'inscrire</h2>
-      <h2 style={{ fontSize: '3em', position: 'absolute', top: '80px', left: '20px' }}>C'est simple et rapide</h2>
+    <div className="signup-page">
+      <div className="header">
+        <h2>S'inscrire</h2>
+        <h2>C'est simple et rapide</h2>
+      </div>
 
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <div className="name-row">
+            <div className="form-group">
+              <label className="text">Nom :</label>
+              <input 
+                type="text" 
+                value={lastName} 
+                onChange={(e) => setLastName(e.target.value)} 
+                placeholder="Entrez votre nom" 
+                required 
+              />
+            </div>
+            <div className="form-group">
+              <label className="text">Prénom :</label>
+              <input 
+                type="text" 
+                value={firstName} 
+                onChange={(e) => setFirstName(e.target.value)} 
+                placeholder="Entrez votre prénom" 
+                required 
+              />
+            </div>
+          </div>
 
-        <div>
-          <label>Nom :</label>
-          <input 
-            type="text" 
-            value={lastName} 
-            onChange={(e) => setLastName(e.target.value)} 
-            placeholder="Entrez votre nom" 
-            required 
-          />
-        </div>
-        <div>
-          <label>Prénom :</label>
-          <input 
-            type="text" 
-            value={firstName} 
-            onChange={(e) => setFirstName(e.target.value)} 
-            placeholder="Entrez votre prénom" 
-            required 
-          />
-        </div>
-        <div>
-          <label>Commune :</label>
-          <input 
-            type="text" 
-            value={commune} 
-            onChange={(e) => setCommune(e.target.value)} 
-            placeholder="Entrez votre commune" 
-            required 
-          />
-        </div>
-        <div>
-          <label>Email :</label>
-          <input 
-            type="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            placeholder="Entrez votre email" 
-            required 
-          />
-        </div>
-        <div>
-          <label>Mot de passe :</label>
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            placeholder="Entrez votre mot de passe" 
-            required 
-          />
-        </div>
-        <div>
-          <label>Confirmer le mot de passe :</label>
-          <input 
-            type="password" 
-            value={confirmPassword} 
-            onChange={(e) => setConfirmPassword(e.target.value)} 
-            placeholder="Confirmez votre mot de passe" 
-            required 
-          />
-        </div>
-        
-        <button type="submit">S'inscrire</button>
-      </form>
+          <div className="form-group">
+            <label className="text">Commune :</label>
+            <input 
+              type="text" 
+              value={commune} 
+              onChange={(e) => setCommune(e.target.value)} 
+              placeholder="Entrez votre commune" 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label className="text">Email :</label>
+            <input 
+              type="email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              placeholder="Entrez votre email" 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label className="text">Mot de passe :</label>
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              placeholder="Entrez votre mot de passe" 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label className="text">Confirmer le mot de passe :</label>
+            <input 
+              type="password" 
+              value={confirmPassword} 
+              onChange={(e) => setConfirmPassword(e.target.value)} 
+              placeholder="Confirmez votre mot de passe" 
+              required 
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Statut :</label>
+            <select value={status} onChange={(e) => setStatus(e.target.value)} required>
+              <option value="" disabled>Sélectionnez votre statut</option>
+              <option value="utilisateur">Utilisateur</option>
+              <option value="actionnaire">Actionnaire</option>
+            </select>
+          </div>
+
+          <div className="form-group checkbox-group">
+            <input 
+              type="checkbox" 
+              checked={termsAccepted} 
+              onChange={(e) => setTermsAccepted(e.target.checked)} 
+              required 
+             id="terms" /* Ajout d'un ID pour l'accessibilité */
+           />
+           <label htmlFor="terms"> J'accepte les <a href="/confidentialite">termes de confidentialité</a>.</label>
+          </div>
+
+          <button type="submit">S'inscrire</button>
+        </form>
+      </div>
     </div>
   );
 };
